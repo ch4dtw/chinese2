02b5d77f17ae8746908ad229aa5356e8f0fd7875
chad <Chad531206@gmail.com>
2016-09-22 14:32:29 +0800

    add restart

--- current/7/user.js
+++ current/7/user.js
@@ -59,5 +59,51 @@ function gameFailed() {
 }
 
 function restart(){
-    location.reload();
+    score = 0;
+    cnt = 0;
+    var right = $('#right');
+    var left = $('#left');
+    var draggable = $('.draggable');
+    $('#score').html(score);
+    $('.Liao').show();
+    $('.fail').hide();
+    draggable.remove();
+    resetDroppable();
+    right.append('<div class="draggable" id="child"><img class="drag_img" src="img/child.jpg" ></div>');
+    right.append('<div class="draggable" id="family"><img class="drag_img" src="img/family.jpg" ></div>');
+    right.append('<div class="draggable" id="price"><img class="drag_img" src="img/price.jpg" ></div>');
+    left.append('<div class="draggable" id="fat"><img class="drag_img" src="img/fat.jpg" ></div>');
+    left.append('<div class="draggable" id="work"><img class="drag_img" src="img/work.jpg" ></div>');
+    left.append('<div class="draggable" id="money"><img class="drag_img" src="img/money.jpg" ></div>');
+    draggable = $('.draggable');
+    draggable.draggable({revert: "invalid"});
+    draggable.draggable('enable');
+}
+
+function resetDroppable(){
+    $('.droppable').droppable('enable', {
+        activeClass: 'ui-state-default',
+        hoverClass: 'ui-state-hover',
+        drop: function (event, ui) {
+            $(this).addClass('ui-state-highlight');
+            ui.draggable.remove();
+            if (Ans.indexOf(ui.draggable.attr('id')) != -1) {
+                applause.play();
+                score += 25;
+                cnt++;
+                $('.Liao').attr('src', 'img/Liao-1.png');
+            }
+            else {
+                boo.play();
+                cnt++;
+                score -= 5;
+            }
+            if (cnt == 4) {
+                $(this).droppable('disable');
+                $('.draggable').draggable('disable');
+                end();
+            }
+            $('#score').html(score);
+        }
+    });
 }
